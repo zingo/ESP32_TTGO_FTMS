aed1b885c4cf9eb38eb7cc72bfc7a4272970a000
Zingo Andersen <zingo@zingo.org>
2022-08-18 23:53:37 +0200

    Fix depricated call in lvgl fluch callback

--- src/ESP32_TTGO_FTMS.cpp
+++ src/ESP32_TTGO_FTMS.cpp
@@ -1154,14 +1154,14 @@ static void toggle_event_handler(lv_event_t * e)
 }
 
 /*** Display callback to flush the buffer to screen ***/
-void display_flush(lv_disp_drv_t * disp, const lv_area_t *area, lv_color_t *color_p)
+void displayFlushCallBack(lv_disp_drv_t * disp, const lv_area_t *area, lv_color_t *color_p)
 {
   uint32_t w = (area->x2 - area->x1 + 1);
   uint32_t h = (area->y2 - area->y1 + 1);
 
   tft.startWrite();
   tft.setAddrWindow(area->x1, area->y1, w, h);
-  tft.pushColors((uint16_t *)&color_p->full, w * h, true);
+  tft.pushPixels((uint16_t *)&color_p->full, w * h, true);
   tft.endWrite();
 
   lv_disp_flush_ready(disp);
@@ -1235,7 +1235,7 @@ void initGFX() {
   lv_disp_drv_init(&disp_drv);
   disp_drv.hor_res = screenWidth;
   disp_drv.ver_res = screenHeight;
-  disp_drv.flush_cb = display_flush;
+  disp_drv.flush_cb = displayFlushCallBack;
   disp_drv.draw_buf = &draw_buf;
   lv_disp_drv_register(&disp_drv);
 
